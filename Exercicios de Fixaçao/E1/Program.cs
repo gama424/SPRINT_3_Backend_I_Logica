b907b24662a20ea3e252fe32b959e8db4e5f02f6
gama424 <tangodown4977@gmail.com>
2021-01-06 16:16:35 -0300

    vamos aplicar os exercícios da lista em anexo em projetos de console

---
+++ Exercicios de Fixaçao/E1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 1");
+            
+            Console.WriteLine("Digite o ano atual");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu ano de nascimento");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            //processamento
+
+            int idade = anoAtual-anoNascimento;
+
+            Console.WriteLine(idade);
+
+            //saida
+
+
+            if(idade>=16){
+                Console.WriteLine("Você pode votar");
+
+            }else{
+                Console.WriteLine("Você nao pode votar");
+            }
+
+
+            
+        }
+    }
+}
---
+++ Exercicios de Fixaçao/E2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gasosa");
+
+            Console.WriteLine("selecione um tipo de combustivel");
+            Console.WriteLine("A- Alcool");
+            Console.WriteLine("G- Gasosa");
+            string combustivel = Console.ReadLine();
+
+            Console.WriteLine("digite a quantidade de combustivel");
+            float quantidadeCombustivel = float.Parse(Console.ReadLine());
+
+
+            float percDesconto, desconto, valorTotal;
+            float precoAlcool = 4f;
+            float precoGasosa = 5f;
+
+            switch (combustivel.ToLower())
+            {
+                case "a":
+                    if(quantidadeCombustivel>20){
+                        percDesconto = 0.05f;
+                    }else{
+                        percDesconto = 0.03f;
+                    }
+
+                    
+                    
+                    valorTotal = (quantidadeCombustivel * precoAlcool);
+                    desconto = (quantidadeCombustivel * precoAlcool)* percDesconto;
+                    Console.WriteLine($"valor a ser pago{valorTotal-desconto}");
+                   
+                    break;
+
+                    case "g":
+                    if(quantidadeCombustivel>20){
+                        percDesconto = 0.6f;
+
+                    }else{
+                        percDesconto = 0.04f;
+
+                    }
+                    valorTotal = (quantidadeCombustivel * precoGasosa);
+                    desconto = (quantidadeCombustivel * precoGasosa) * percDesconto;
+                    Console.WriteLine($"valor a ser pago{valorTotal-desconto}");
+                    break;
+
+                default:
+                Console.WriteLine("opçao invalida");
+                    break;
+            }
+            
+
+            float AplicarDesconto(float litros, float preco, float perc1, float perc2){
+                if(litros>20){
+                    float valorTotal = litros * preco;
+                    float desconto = (litros* preco)* perc1
+                    return valorTotal - desconto;
+
+                }else{
+                    float valorTotal = litros * preco;
+                    float desconto = (litros* preco)* perc2
+                    return valorTotal - desconto;
+                }
+            }
+
+        }
+    }
+}
