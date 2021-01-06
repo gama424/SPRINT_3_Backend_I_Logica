c234da7bbdb1b63b69c12b2fd392670bc99881dc
gama424 <tangodown4977@gmail.com>
2021-01-06 16:21:46 -0300

    vamos aplicar os exercícios focando nas estruturas de repetição aprendidos na aula passada

---
+++ Estruturas de Repetiçao/Salário.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace atividades
+{
+    class Salário
+    {
+        static void Main(string[] args)
+        {
+            
+          float salario, reajuste;
+        float porcentagem = (float)(100.0 * 0.30);
+           
+            Console.Write("digite seu salário: ");
+            salario = float.Parse(Console.ReadLine());
+
+
+
+            if(salario<=500){
+                reajuste = salario + porcentagem;
+                Console.Write("o seu salario foi reajustado para:" + reajuste);
+            }
+                else{
+                    Console.Write("voce nao tem direito ao reajuste");
+                }
+        
+        }
+    }
+}
