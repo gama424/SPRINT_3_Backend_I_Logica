2229c2b0f1eca4f3d8d8695e272c5189e1c48d40
gama424 <tangodown4977@gmail.com>
2021-01-06 16:28:32 -0300

    vamos aprender o que são estruturas de repetição (laços) e seus tipos: while, do while, for.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace atividades
+{
+    class Tabuada
+    {
+        static void Main(string[] args)
+        {
+         
+                
+            // for (var contador = 1; contador < 11; contador++)
+            // {
+            //     Console.WriteLine("digite um valor");
+            //     int valor = int.Parse(Console.ReadLine());
+            //     Console.WriteLine($"Você digitou: {valor}");
+
+                
+                
+            // }
+            int conta,i , numero;
+
+           
+            Console.Write("Digite um número para ver a tabuada: ");
+            numero = int.Parse(Console.ReadLine());
+
+            
+            for ( i = 1; i <= 10; ++i)
+            {
+                conta =  numero * i;
+                Console.WriteLine(numero + " X " + i + " = " + conta);
+                
+            }
+
+
+        }
+    }
+}
