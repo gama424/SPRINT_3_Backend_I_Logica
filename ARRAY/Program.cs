85c0ce0e5b51c91890d8a81b68abc37bf3742ecb
gama424 <tangodown4977@gmail.com>
2021-01-06 16:51:20 -0300

    vamos aprender sobre o conceito e aplicação de arrays, como ler e atribuir eles com os laços for e foreach

---
+++ ARRAY/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array e Vetor");
+        
+        string [] nomes = new string[3];  
+        int[] idades = new int[3];
+
+        for (var contador = 0; contador < 3; contador++)
+        {
+        Console.WriteLine($"Digite o {contador + 1}º nome: "); // interpolação
+        nomes[contador] = Console.ReadLine();
+        Console.WriteLine("Digite a idade");
+        idades[contador] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Nome da rapaziada");
+        for (var contador = 0; contador < 3; contador++)
+        {
+
+            Console.WriteLine("Nome: "+nomes[contador]); //contador
+            Console.WriteLine("Idade: " + idades[contador]);
+        }
+        
+        }        
+    }
+}
